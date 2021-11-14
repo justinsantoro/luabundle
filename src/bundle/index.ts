17fa3c3d4b35e73c0732c04b4d943a0c5e2b1c18
Justin Santoro <jzsantoro14@gmail.com>
2021-11-14 15:38:57 -0500

    embed runtime.lua as string literal

--- src/bundle/index.ts
+++ src/bundle/index.ts
@@ -1,10 +1,4 @@
 import {
-	resolve as resolvePath,
-	fromFileUrl,
-	dirname
-} from "https://deno.land/std@0.114.0/path/mod.ts";
-
-import {
 	Module,
 	ModuleMap,
 } from './module.ts'
@@ -45,7 +39,49 @@ export function bundleString(lua: string, options: Options = {}): string {
 	}
 
 	const identifiers = realizedOptions.identifiers
-	const runtime = Deno.readFileSync(resolvePath(dirname(fromFileUrl(import.meta.url)), './runtime.lua'))
+
+	const runtime = `(function(superRequire)
+	local loadingPlaceholder = {[{}] = true}
+
+	local register
+	local modules = {}
+
+	local require
+	local loaded = {}
+
+	register = function(name, body)
+		if not modules[name] then
+			modules[name] = body
+		end
+	end
+
+	require = function(name)
+		local loadedModule = loaded[name]
+
+		if loadedModule then
+			if loadedModule == loadingPlaceholder then
+				return nil
+			end
+		else
+			if not modules[name] then
+				if not superRequire then
+					local identifier = type(name) == 'string' and '\"' .. name .. '\"' or tostring(name)
+					error('Tried to require ' .. identifier .. ', but no such module has been registered')
+				else
+					return superRequire(name)
+				end
+			end
+
+			loaded[name] = loadingPlaceholder
+			loadedModule = modules[name](require, loaded, register, modules)
+			loaded[name] = loadedModule
+		end
+
+		return loadedModule
+	end
+
+	return require, loaded, register, modules
+end)`
 
 	let bundle = ''
 
